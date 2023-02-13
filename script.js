b1b356d456aa66a17d80b5535bf51eb747ef67d6
kishore-klassy <94288452+kishore-klassy@users.noreply.github.com>
2023-02-13 21:47:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var $grid = $('.collection-list').isotope({
+  
+});
+
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
